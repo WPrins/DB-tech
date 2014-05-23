c7772da3b321fff3e3573d651e32e59eae3760f1
P.J.H. van Heck <P.J.H. van Heck@S081285>
2014-05-23 11:49:54 +0200

    Implemented readData and started making histograms

---
+++ src/dbtech/Data.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbtech;
+
+/**
+ *
+ * @author P.J.H. van Heck
+ */
+public class Data {
+    
+    
+    public static void main (){
+        
+    }
+    
+}
